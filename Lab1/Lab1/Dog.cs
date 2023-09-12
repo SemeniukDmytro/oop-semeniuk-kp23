ba3136a8be305335695d87da7b1bbd9ea07c5baf
DmytroSemeniuk <mr.semenyuk.dmitro@gmail.com>
2023-09-12 11:33:16 -0500

    added forced GC invoking and testing for its methods, implemented IDisposable

---
+++ Lab1/Lab1/Dog.cs
@@ -0,0 +1,62 @@
+﻿namespace Lab1
+{
+    internal class Dog : HomePet
+    {
+
+        public List<string> Commands { get; set; }
+        public string Alias { get; set; }
+        private bool isHouseKept { get; set; }
+        string[] dogAdjectives = new string[]
+        {
+            "Loyal",
+            "Playful",
+            "Energetic",
+            "Affectionate",
+            "Obedient",
+            "Friendly",
+            "Intelligent",
+            "Devoted",
+            "Protective",
+            "Curious",
+            "Adorable",
+            "Faithful",
+            "Gentle",
+            "Alert",
+            "Trustworthy",
+            "Social",
+            "Agile",
+            "Brave",
+            "Mischievous",
+            "Spirited"
+        };
+
+        public Dog(string name, string qrCodeNumber, int age, int ownerPhoneNumber, bool isHouseKept, List<string> commands, string alias)
+        : base(name, qrCodeNumber, age, ownerPhoneNumber)
+        {
+            Alias = alias;
+            Commands = commands;
+            this.isHouseKept = isHouseKept;
+        }
+
+        public void ShowCommands()
+        {
+            foreach (var command in Commands)
+            {
+                Console.WriteLine(command);
+            }
+        }
+        public string TeachACommand(string command)
+        {
+            if (isHouseKept && _age <= 10)
+                return "Good job, now dog knows a new command";
+            else
+                return new Random().Next(1) > 0.5 ? "There were some problems, but dog learned a new command"
+                    : "Dog went away";
+
+        }
+        public override string GetAlias(string name, int age)
+        {
+            return $"{dogAdjectives[new Random().Next(dogAdjectives.Length)]} {name}";
+        }
+    }
+}
--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Lab1;
+
+public class Program
+{
+    private const int maxGarbage = 100;
+    static void Main(string[] args)
+    {
+
+        Cat bob = new Cat("Bonny", "1234567", 25, 1999, new List<string>() { "chin", "back" });
+        bob.ShowPlacesToStroke();
+        Console.WriteLine(bob.GetInfoAboutPet("1234567"));
+        using (var pet = new HomePet("Fluffy", "1234567", 5, 1234567))
+        {
+            pet.GetInfoAboutPet(pet.QrCodeNumber);
+        }
+        MyGarbageCollector mgb = new MyGarbageCollector();
+        mgb.ShowMemoryUsage();
+    }
+    static void MakeSomeGarbage()
+    {
+        HomePet tolya;
+        for (int i = 0; i < maxGarbage; i++)
+        {
+            tolya = new HomePet("tolya", "1234567", 16, 229299);
+            Thread.Sleep(1);
+        }
+    }
+}
+
+
