5f5ae369abe07520e1442582b20effa412ac3c0c
DmytroSemeniuk <mr.semenyuk.dmitro@gmail.com>
2023-09-14 11:42:11 -0500

    task2 done

---
+++ lab3/task2/task2/Minutes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    internal class Minutes
+    {
+        private int _withinUkraine;
+        private int _inRoaming;
+        public int InRoaming { get => _inRoaming;}
+        public int WithinUkraine { get => _withinUkraine; }
+        public Minutes(int withinUkraine, int inRoaming)
+        {
+            _inRoaming = inRoaming;
+            _withinUkraine = withinUkraine;
+        }
+
+        
+
+    }
+}
---
+++ lab3/task2/task2/SMS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    internal class SMS
+    {
+        private int _withinUkraine;
+        private int _inRoaming;
+        public int InRoaming { get => _inRoaming; }
+        public int WithinUkraine { get => _withinUkraine; }
+        public SMS(int withinUkraine, int inRoaming)
+        {
+            _inRoaming = inRoaming;
+            _withinUkraine = withinUkraine;
+        }
+    }
+}
